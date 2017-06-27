fc9bfe6b70b3323da7db304e8b4788d4dc0d9943
Jailson Dias <jcd2@cin.ufpe.br>
2017-06-27 01:57:26 -0300

    adcionado a rede neural

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import csv
 from os import listdir
 from os.path import isfile, join
 from time import sleep
+import operator
 
 # Imports the Google Cloud client library
 from google.cloud import vision
 
 path = os.path.join(os.path.dirname(__file__),'images')
 images = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+
+
+images = sorted(images, key=lambda x: int(x.split('.')[0][-3:]))
+
+print (images)
+
 
 # print (images)
 
@@ -18,27 +25,30 @@ columns = ['leg','shoulder','thigh','joint','muscle','structure','abdomen',
             'standing','photo shoot','trunk','sport venue','leggings','tights','shorts',
             'photography','beauty','model','undergarment']
 
-csvfile =  open('data_input.csv', 'a')
+linhas = 0
 
-writer = csv.writer(csvfile, delimiter=',')
-linhas = 5
-vision_client = vision.Client('plenary-network-146618')
-
-while linha > 0:
+while linhas == 0:
+    vision_client = vision.Client('plenary-network-146618')
+    csvfile =  open('data_input.csv', 'a')
+    writer = csv.writer(csvfile, delimiter=',')
+    
     csvread =  open('data_input.csv', 'r')
     read = csv.reader(csvread, delimiter=',')
 
     linhas = len(list(csvread))
 
-    csvread.close()
-
+    print (linhas, "\n\n")
     if linhas<=0:
         writer.writerow(columns)
-
+        linhas = 1
+        print(columns, "column")
+    print ("pegando imagem")
+    csvread.close()
     # Instantiates a client
     try:
         for file_name in images[linhas-1:]:
             # Loads the image into memory
+            print ("requisitando imagem", file_name)
             image_file =  io.open(file_name, 'rb')
             content = image_file.read()
             image = vision_client.image(
@@ -49,7 +59,7 @@ while linha > 0:
         #     # properties = image.detect_properties()
 
 
-        #     # print('Labels:')
+            print('Labels:')
         #     # Performs label detection on the image file
             labels = image.detect_labels()
 
@@ -68,6 +78,6 @@ while linha > 0:
     except Exception:
         linhas = 0
         print ("error")
+        sleep(5)
 
-csvfile.close()
-csvlabel.close()
+    csvfile.close()
--- train.py
+++ train.py
@@ -2,13 +2,28 @@ import csv
 import numpy as np
 from sklearn.neural_network import MLPRegressor
 
-num_feats = 1
+num_feats = 26
 
-file = open('trainning_scores.csv', 'r')
+file = open('data_input.csv', 'r')
 reader = csv.reader(file, delimiter=',')
-reader = np.array(list(reader))
-data = reader[:,0:num_feats]
-target = reader[:, num_feats]
 
+followlike = open('followlikes.csv', 'r')
+readerfollowlike = csv.reader(followlike, delimiter=',')
+
+reader = np.array(list(reader))
+readerfollowlike = np.array(list(readerfollowlike))
+
+dados = np.c_[reader[1:], readerfollowlike[1:]]
+
+
+
+
+
+data = dados[:,:num_feats].astype(np.float)
+target = dados[:, num_feats].astype(np.float)
+# print (data.tolist())
+# print ("\n\n\n",target.tolist())
 reg = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 reg.fit(data, target)
+
+print (reg)
