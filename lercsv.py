d2687abe1ff3a84fd62e6bf55d24f0651c5de868
Jailson Dias <jcd2@cin.ufpe.br>
2017-06-27 07:15:44 -0300

    alterações

---
+++ lercsv.py
@@ -0,0 +1,51 @@
+import json
+import csv
+import operator
+
+def ler():
+    csvread =  open('trainning.csv', 'r')
+    read = csv.reader(csvread, delimiter=',')
+
+    tags = {}
+
+    for row in read:
+        for tag in row:
+            if tag in tags:
+                tags[tag] += 1
+            else:
+                tags[tag] = 1
+
+    tags = sorted(tags.items(), key=operator.itemgetter(1), reverse=True)
+    print(tags)
+    csvread.close()
+
+def escrever():
+    columns = ['adult','caucasian','person','people','attractive','lifestyle',
+    'happy','smile','pretty','model','human','fashion','portrait','lady', 'body',
+    'women', 'smiling', 'black', 'happiness', 'sexy', 'one', 'face', 'hair', 
+    'posing', 'cute', 'healthy', 'clothing'
+    , 'youth', 'health', 'looking', 'fitness', 'studio', 'fit', 'brunette'
+    , 'fun', 'slim', 'pose', 'holding', 'cheerful', 'sensual', 'glamour']
+    
+    # csvread =  open('output-api-imagga.json', 'r')
+    # read = csv.reader(csvread, delimiter=',')
+    file = open('output-api-imagga.json').read()
+    j = json.loads(file)
+
+    csvfile =  open('data_input.csv', 'a')
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(columns)
+    for linha in j:
+        # print (linha)
+        descs = linha["results"][0]["tags"]
+        # print (descs)
+        row = []
+        for column in columns:
+            if len([row.append(x["confidence"]) for x in descs if x['tag'] == column]) <= 0:
+                row.append(0)
+
+        # print (row)
+        writer.writerow(row)
+    csvfile.close()
+
+escrever()
--- train.py
+++ train.py
@@ -1,29 +1,118 @@
 import csv
 import numpy as np
 from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import scale
 
-num_feats = 26
+import random
 
+num_feats = 42
+margem_acerto = 100
+
+# lendo os dados vindo do google
 file = open('data_input.csv', 'r')
 reader = csv.reader(file, delimiter=',')
 
+# lendo os dados de follow e likes
 followlike = open('followlikes.csv', 'r')
 readerfollowlike = csv.reader(followlike, delimiter=',')
 
+# colocando os dados em um array do numpy
 reader = np.array(list(reader))
 readerfollowlike = np.array(list(readerfollowlike))
 
+# juntando os dados vindos do google com os seguidores e curtidas
 dados = np.c_[reader[1:], readerfollowlike[1:]]
 
 
+# zero = []
+# for i, d in enumerate(dados):
+#     if np.count_nonzero(d == '0') > 18:
+#         zero.append(i)
 
+# dados = np.array(filter(lambda x: np.count_nonzero(x == '0') >20, dados))
+# print (dados)
+# dados = np.delete(dados, zero, 0)
 
+# zero = []
+# for i, d in enumerate(dados[:,-2]):
+#     if float(d)>4000000 or float(d)<100000:
+#         zero.append(i)
 
+# print (zero)
+# dados = np.delete(dados, zero, 0)
+print (len(dados))
+# print (len(dados))
+
+# """
+# funcao utilizada para normalizar os seguidores
+def normalize(lista):
+    mini = min(lista)
+    maxi = max(lista)
+    return [(x-mini)/(maxi-mini) for x in lista]
+
+# sepadando os inputs e outputs e convertendo os dados para float
 data = dados[:,:num_feats].astype(np.float)
 target = dados[:, num_feats].astype(np.float)
-# print (data.tolist())
-# print ("\n\n\n",target.tolist())
-reg = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-reg.fit(data, target)
 
-print (reg)
+# normalizando os seguidores
+data = np.c_[data[:,:-1],np.array(normalize(data[:,-1]))]
+# activation= 'tanh'
+
+maior = 0
+valores = [65,46,35,20,8]
+
+vezes = 0
+
+def treinamento(data, target):
+    global vezes, valores, maior
+
+    # numeros = []
+    # quant = random.randint(2,15)
+    # for i in range(0,quant):
+    #     numeros.append(random.randint(3,150))
+
+    # Treinando a rede neural
+    reg = MLPRegressor(solver='lbfgs',alpha=1e-5, learning_rate="constant", hidden_layer_sizes=valores, random_state=2)
+    reg.fit(data[10:], target[10:])
+
+
+    # print (reg.loss_)
+    # verifica quantos acertos no conjunto de treinamento estao classificados errados
+    resultado = reg.predict(data[10:]).tolist()
+    targetlist = target[10:].tolist()
+    for i in range(0,len(resultado)):
+        if abs(resultado[i] - targetlist[i]) < margem_acerto:
+            print ('Resultado: %.2f, Esperado: %.2f (Certo)' % (resultado[i], targetlist[i]))
+        else:
+            print ('Resultado: %.2f, Esperado: %.2f         (Errado)' % (resultado[i], targetlist[i]))
+
+
+    print ("Conjunto de Teste")
+
+    # print (data[:5], "\n\n")
+    # print (np.c_[data[:5],data[28:]], "\n\n")
+    # verifica quantos acertos no conjunto de teste estao classificados errados
+    resultado = reg.predict(data[:10]).tolist()
+    # print (data[:10])
+    targetlist = target[:10].tolist()#  + target[:10].tolist()
+    # print (reg.score(data[28:],target[28:]))
+    certos = 0
+    for i in range(0,len(resultado)):
+        if abs(resultado[i] - targetlist[i]) < margem_acerto:
+            certos += 1
+            print ('Resultado: %.2f, Esperado: %.2f (Certo)' % (resultado[i], targetlist[i]))
+        else:
+            print ('Resultado: %.2f, Esperado: %.2f         (Errado)' % (resultado[i], targetlist[i]))
+
+    if certos > maior:
+        valores = numeros
+        maior = certos
+        print (valores, "melhor")
+    
+    # vezes += 1
+    # if vezes < 20:
+    #     treinamento(data, target)
+
+treinamento(data, target)
+print ("valore", valores, "quantidade",maior)
+# """
